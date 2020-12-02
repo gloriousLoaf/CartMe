b1d59cfe53ebcca417d67fd15a2cab53c9047d41
gloriousLoaf <davidmetcalfwork@gmail.com>
2020-12-02 11:45:00 -0800

    Created basic backend with express server.

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { fchown } = require('fs/promises');
+const { func } = require('prop-types');
+const app = express();
+const products = require('./data/products');
+
+app.get('/', (req, res) => {
+  res.send('API is running.');
+});
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find(prod => prod._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(5000, console.log('Server running on port 5000'));
