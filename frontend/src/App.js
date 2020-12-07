04caffde69bc5699c13128b48403548f5edbd47e
gloriousLoaf <davidmetcalfwork@gmail.com>
2020-12-07 14:39:40 -0800

    Created  add to cart function in ProductView. CartView is dummy for now, but history push is working, with product id & qty.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeView from './views/HomeView';
 import ProductView from './views/ProductView';
+import CartView from './views/CartView';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Container>
           <Route path="/" component={HomeView} exact />
           <Route path="/product/:id" component={ProductView} />
+          <Route path="/cart/:id?" component={CartView} />
         </Container>
       </main>
       <Footer />
