dc175e282cd30a26bd97762f96e3fbaf48c1387b
gloriousLoaf <davidmetcalfwork@gmail.com>
2020-12-10 14:02:39 -0800

    Authentication: Created userController, added bcrypt method matchPassword to userModel. Works, but haven't implemented tokens yet.

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 /* USER MODEL */
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = mongoose.Schema({
   name: {
@@ -23,6 +24,11 @@ const userSchema = mongoose.Schema({
 }, {
   timestamps: true
 });
+
+// bcrypt method for passwords
+userSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 
