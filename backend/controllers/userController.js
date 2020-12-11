817923fd241b1a8c532737bc9e7fb589eed2d2fa
gloriousLoaf <davidmetcalfwork@gmail.com>
2020-12-11 10:11:20 -0800

    Authentication: Basic JWT setup built, POST/aoi/users/login working with token. Also fixed ERR_HTTP_HEADERS_SENT coming from errorMiddleware.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 /* USER CONTROLLER */
 import asyncHandler from 'express-async-handler';
+import generateToken from '../utils/generateToken.js';
 import User from '../models/userModel.js';
 
 // @desc      Auth user & get token
@@ -17,7 +18,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null
+      token: generateToken(user._id)
     })
   : res.status(401);
     throw new Error('Invalid email or password');
--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -8,6 +8,10 @@ const notFound = (req, res, next) => {
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode);
+  // found this 'if' on SO, fixes console err [ERR_HTTP_HEADERS_SENT]
+  if (err) {
+    return res.send();
+  }
   res.json({
     message: err.message,
     stack: process.env.NODE_ENV === 'production' ? null : err.stack
