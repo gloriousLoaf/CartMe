ab638849725d8bd95bd25ce4e17eb964c5803df5
gloriousLoaf <davidmetcalfwork@gmail.com>
2021-03-08 13:34:30 -0800

    Created routes & middleware for multer image file uploads, mounted in server.

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,48 @@
+/* FILE UPLOAD ROUTE */
+import path from 'path';
+import express from 'express';
+import multer from 'multer';
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename(req, file, cb) {
+    cb(
+      null,
+      `${file.fieldname}- ${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+function checkFileType(file, cb) {
+  const filetypes = /jpg|jpeg|png/;
+  const fileExtname = filetypes.test(
+    path.extname(file.originalname).toLowerCase()
+  );
+  // match 'image/png' etc.
+  const fileMimetype = filetypes.test(file.mimetype);
+
+  if (fileExtname && fileMimetype) {
+    return cb(null, true);
+  } else {
+    cb('JPG / JPEG or PNG only.');
+  }
+}
+
+// middleware: ensure only image files
+const upload = multer({
+  storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+});
+
+router.post('/', upload.single('image'), (req, res) => {
+  // return db path to file
+  res.send(`/${req.file.path}`);
+});
+
+export default router;
