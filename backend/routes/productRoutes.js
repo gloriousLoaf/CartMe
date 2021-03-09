6fff8f5e367490dee80c337c138c34479e90c205
gloriousLoaf <davidmetcalfwork@gmail.com>
2021-03-09 10:59:32 -0800

    Created controller & route for product reviews.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,12 +6,14 @@ import {
   deleteProductById,
   updateProductById,
   createProduct,
+  createProductReview,
 } from '../controllers/productController.js';
 import { protect, isAdmin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.route('/').get(getProducts).post(protect, isAdmin, createProduct);
+router.route('/:id/reviews').post(protect, createProductReview);
 router
   .route('/:id')
   .get(getProductById)
