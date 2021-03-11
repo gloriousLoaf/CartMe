e309fbace0f589245686d954ec7f8dd9b8628a1b
gloriousLoaf <davidmetcalfwork@gmail.com>
2021-03-10 17:30:01 -0800

    Still on heroku stuff...

--- backend/server.js
+++ backend/server.js
@@ -37,12 +37,10 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 // production vs development
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
 
   app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '..', 'frontend', 'build', 'index.html')
-    )
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   );
 } else {
   app.get('/', (req, res) => {
