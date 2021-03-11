232f337464069c5789b884bdefc01e022f321a21
gloriousLoaf <davidmetcalfwork@gmail.com>
2021-03-10 16:37:46 -0800

    Prepping for Heroku.

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,6 @@ process.env.NODE_ENV === 'development' && app.use(morgan('dev'));
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running.');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -38,6 +34,19 @@ app.get('/api/config/paypal', (req, res) =>
 // cuz ES modules
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+// production vs development
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running.');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
